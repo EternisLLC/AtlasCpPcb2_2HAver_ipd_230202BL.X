7a66cf5757dcec03b20df78be76fce89f0b41054
AV <balaba_a@eternis.ru>
2023-02-03 09:58:04 +0300

    Initial commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=AtlasCpPcb2_2HAver_ipd_230202BL.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/AtlasCpPcb2_2HAver_ipd_230202BL.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=atlascppcb22haveripd230202bl.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/atlascppcb22haveripd230202bl.x.tar
